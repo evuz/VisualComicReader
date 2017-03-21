649b0271cea0e0c584492c9c7c6e445961b79a16
evuz <jesus_jgb_45@hotmail.com>
2017-03-21 21:30:43 +0100

    Remove unnecesary files & create layout

---
+++ src/app/components/index.js
@@ -0,0 +1,5 @@
+import Layout from './layout/layout';
+
+export {
+    Layout
+};
---
+++ src/app/components/layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './layout.scss'
+
+const Layout = () => (
+    <div className='layout'>
+        <div className='header_nav'></div>
+        <div className='lateral_nav'></div>
+        <div className='container'></div>
+    </div>
+);
+
+export default Layout;
