990d29ced94f0a45b2fc0025dabe065c2e350468
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-04-04 00:02:02 +0200

    Drag scroll in image

--- src/app/components/reader/reader.js
+++ src/app/components/reader/reader.js
@@ -1,7 +1,47 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 class Reader extends Component {
+    constructor() {
+        super();
+        this.state = {
+            clicked: false,
+            cursorY: 0,
+            cursorX: 0
+        }
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+    }
+
+    handleMouseUp() {
+        this.setState({
+            clicked: false
+        })
+    }
+
+    handleMouseDown(e) {
+        this.setState({
+            clicked: true,
+            cursorX: e.pageX,
+            cursorY: e.pageY
+        })
+    }
+
+    handleMouseMove(e) {
+        const { clicked, cursorX, cursorY } = this.state;
+        if (clicked) {
+            let { reader } = this.refs;
+            const { pageX, pageY } = e;
+            reader.scrollLeft -= -cursorX + pageX;
+            reader.scrollTop -= -cursorY + pageY;
+            this.setState({
+                cursorX: pageX,
+                cursorY: pageY
+            })
+        }
+    }
+
     imgSize() {
         const {
             fullHeight,
@@ -24,10 +64,14 @@ class Reader extends Component {
     render() {
         const { files, directory, page } = this.props.reader;
         const { twoColumns, percentSize } = this.props.options;
+        const { clicked } = this.state;
         const { height, width } = twoColumns ? { height: percentSize + '%' } : this.imgSize();
 
         return files.length ? (
-            <div className="reader" >
+            <div
+                className="reader"
+                ref="reader"
+            >
                 {twoColumns ?
                     <div className="doblePage"
                         style={{
@@ -35,23 +79,36 @@ class Reader extends Component {
                         }}
                     >
                         <img
-                            className="left"
+                            className={clicked ? 'left drag' : 'left'}
                             src={directory + files[page]}
                             alt=""
+                            draggable="false"
+                            onMouseDown={this.handleMouseDown}
+                            onMouseUp={this.handleMouseUp}
+                            onMouseMove={this.handleMouseMove}
                         />
                         <img
-                            className="right"
+                            className={clicked ? 'right drag' : 'right'}
                             src={directory + files[page + 1]}
                             alt=""
+                            draggable="false"
+                            onMouseDown={this.handleMouseDown}
+                            onMouseUp={this.handleMouseUp}
+                            onMouseMove={this.handleMouseMove}
                         />
                     </div>
                     : <img
+                        className={clicked ? 'drag' : ''}
                         src={directory + files[page]}
                         alt=""
                         style={{
                             height: height,
                             width: width
                         }}
+                        onMouseDown={this.handleMouseDown}
+                        onMouseUp={this.handleMouseUp}
+                        onMouseMove={this.handleMouseMove}
+                        draggable="false"
                     />
                 }
             </div>
