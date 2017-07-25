e2bdec6fd775f94863f6ddaf9aefb6f2d77c73e1
evuz <jesus_jgb_45@hotmail.com>
2017-07-25 12:19:29 +0200

    windowState reducer: add fetching variable

--- src/renderer/app/reducers/windowState/actions.js
+++ src/renderer/app/reducers/windowState/actions.js
@@ -1,8 +1,15 @@
-import { SET_FULL_SCREEN } from './actionTypes';
+import { SET_FULL_SCREEN, SET_FETCHING } from './actionTypes';
 
 export function setFullScreen(value) {
   return {
     type: SET_FULL_SCREEN,
     fullScreen: value,
+  };
+}
+
+export function setFetching(state) {
+  return {
+    type: SET_FETCHING,
+    state,
   };
 }
--- src/renderer/app/reducers/windowState/index.js
+++ src/renderer/app/reducers/windowState/index.js
@@ -1,20 +1,18 @@
-import { SET_FULL_SCREEN } from './actionTypes';
+import { SET_FULL_SCREEN, SET_FETCHING } from './actionTypes';
 
 export * from './actions';
 
-function options(
-  state = {
-    fullScreen: false,
-  },
-  action,
-) {
+const initialState = {
+  fullScreen: false,
+  fetching: false,
+};
+
+function options(state = initialState, action) {
   switch (action.type) {
-    case SET_FULL_SCREEN: {
-      const { fullScreen } = action;
-      return Object.assign({}, state, {
-        fullScreen,
-      });
-    }
+    case SET_FULL_SCREEN:
+      return Object.assign({}, state, { fullScreen: state.fullScreen });
+    case SET_FETCHING:
+      return Object.assign({}, state, { fetching: action.state });
     default:
       return state;
   }
