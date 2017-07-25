0921423f1e88b7f90d4f6bf1ab896a88c69e739d
evuz <jesus_jgb_45@hotmail.com>
2017-07-25 10:50:12 +0200

    Recover feature: change comics automatically

--- src/renderer/app/reducers/reader/actions.js
+++ src/renderer/app/reducers/reader/actions.js
@@ -1,3 +1,5 @@
+import { ipcRenderer } from 'electron';
+
 import {
   SET_DIRECTORY,
   SET_FILES,
@@ -30,7 +32,8 @@ export function nextPage() {
     const { reader: { page: currentPage, files }, options: { twoColumns } } = getState();
     const newPage = twoColumns ? currentPage + 2 : currentPage + 1;
     if (newPage < files.length) dispatch(setPage(newPage));
-    else if (twoColumns && newPage === files.length + 1) dispatch(setPage(newPage - 1));
+    else if (twoColumns && newPage === files.length) dispatch(setPage(newPage - 1));
+    else ipcRenderer.send('next-file');
   };
 }
 
@@ -38,7 +41,8 @@ export function previousPage() {
   return (dispatch, getState) => {
     const { reader: { page: currentPage }, options: { twoColumns } } = getState();
     const newPage = twoColumns ? currentPage - 2 : currentPage - 1;
-    if (newPage > 0) dispatch(setPage(newPage));
+    if (currentPage === 0) ipcRenderer.send('previous-file');
+    else if (newPage > 0) dispatch(setPage(newPage));
     else dispatch(setPage(0));
   };
 }
