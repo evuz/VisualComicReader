a6d50a5c96ba5545cc7bd135d31dd5caebb40144
evuz <jesus_jgb_45@hotmail.com>
2017-07-25 08:56:18 +0200

    Store: use middlewares, redux-thunk & redux-dev-tools

--- src/renderer/app/configureStore.js
+++ src/renderer/app/configureStore.js
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import reader from './reducers/reader';
 import options from './reducers/options';
 import windowState from './reducers/windowState';
@@ -10,7 +12,17 @@ function configureStore() {
     windowState,
   });
 
-  return createStore(appReducers);
+  let enhacer;
+  if (process.env.NODE_ENV === 'development') {
+    enhacer = compose(
+      applyMiddleware(thunk),
+      window.devToolsExtension ? window.devToolsExtension() : f => f,
+    );
+  } else {
+    enhacer = applyMiddleware(thunk);
+  }
+
+  return createStore(appReducers, enhacer);
 }
 
 export default configureStore;
