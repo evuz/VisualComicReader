2f7521a1a8a0dc35bed76aa063904c2042e2c3f0
Jesus Gomez Bellido <jesus_jgb_45@hotmail.com>
2020-03-28 11:42:56 +0100

    chore(electron): add dmg package

--- packages/scripts/src/install-deps.js
+++ packages/scripts/src/install-deps.js
@@ -11,6 +11,11 @@ const paths = require('./paths');
 const PACKAGING_FOLDER = 'out';
 const PACKAGING_PATH = path.resolve(__dirname, paths.electron, PACKAGING_FOLDER);
 
+async function getAppName() {
+  const pkg = JSON.parse(await utils.promisify(fs.readFile)(path.resolve(__dirname, paths.electron, 'package.json'), 'utf-8'));
+  return pkg.productName;
+}
+
 module.exports = async function installDeps() {
   const spinner = ora('Installing dependencies').start();
   const existDistFolder = await utils.promisify(fs.exists)(PACKAGING_PATH);
@@ -19,8 +24,8 @@ module.exports = async function installDeps() {
     console.error(chalk.bold.red('You must package app before'));
     process.exit(1);
   }
-
-  const pkgs = await utils.promisify(glob)(path.join(PACKAGING_PATH, '/**/package.json'));
+  const appName = await getAppName();
+  const pkgs = await utils.promisify(glob)(path.join(PACKAGING_PATH, `${appName}*/**/package.json`));
   const pkg = pkgs[0];
   const pkgPath = path.dirname(pkg);
   await utils.promisify(exec)('npm install --only=prod', { cwd: pkgPath });
