0d19a033ac1338b447a32fd8a2a1b63de8a82ae9
Jesús Gómez <jesgombel@outlook.es>
2021-04-14 17:37:59 +0200

    chore(webapp): update packages

--- packages/webapp/vite.config.js
+++ packages/webapp/vite.config.js
@@ -2,12 +2,12 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   plugins: [reactRefresh()],
   optimizeDeps: {
-    include: ['rxjs/operators']
+    include: ['rxjs/operators'],
   },
   build: {
-    base: '.',
-    outDir: 'build'
-  }
+    outDir: 'build',
+  },
 })
